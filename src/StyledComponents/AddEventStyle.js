938fbe6c6d3ad4842986f053f041c9831c42b7ac
Jonathan <jonathanvantu@gmail.com>
2019-12-18 17:13:17 +0100

    fix search

--- src/StyledComponents/AddEventStyle.js
+++ src/StyledComponents/AddEventStyle.js
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export default styled.div`
 
 /* background-image: url('https://images.pexels.com/photos/954547/pexels-photo-954547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'); */
-background-color: #00000099;
-height: 100%; /* You must set a specified height */
-  background-position: center; /* Center the image */
-  background-repeat: no-repeat; /* Do not repeat the image */
+/* background-color: #00000099; */
+background-color: #142427;
+height: 100%;
+  background-position: center; 
+  background-repeat: no-repeat;
   background-size: cover;
 
   overflow-x:hidden;
